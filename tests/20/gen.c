10cf16cef227ab789789af279e731bd4fd8943fd
Anton Saykin <saikin.ao@phystech.edu>
2024-02-21 01:59:18 +0300

    bug fixed

--- tests/20/gen.c
+++ tests/20/gen.c
@@ -4,10 +4,11 @@ int main() {
 	int result;
 	result = AddTwoNumbers(5, 7);
 	printf("The sum is: ");
-	printf("%d",result)printf("\n");
+	printf("%d",result);
+	printf("\n");
 	}
 int AddTwoNumbers(int a, int b){
-	int _res2fb19146;
-	_res2fb19146 = a+b;
-	return _res2fb19146;
+	int _resf3bbb2c6;
+	_resf3bbb2c6 = a+b;
+	return _resf3bbb2c6;
 	}
