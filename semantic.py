7a09021169f7090a4ff0906bc6781f7eaecf316d
Anton Saykin <saikin.ao@phystech.edu>
2024-02-24 09:00:45 +0300

    bug fix

--- semantic.py
+++ semantic.py
@@ -25,6 +25,11 @@ safe_funcs = [
     'writeln'
 ]
 
+dec_type_assosiations = {
+    'real' : 'integer',
+    'string': 'char'
+}
+
 class SemanticAnalyzer:
     def __init__(self):
         self.symbol_table = {
@@ -34,6 +39,11 @@ class SemanticAnalyzer:
         }
 
         self.func_table = {
+            'ord' : [
+                FuncVar('ord', 'integer', '_', ['integer']),
+                FuncVar('ord', 'integer', '_', ['char']),
+                FuncVar('ord', 'integer', '_', ['boolean']),
+                ],
         }
 
         self.scope_stack = []
@@ -73,8 +83,9 @@ class SemanticAnalyzer:
             # Проверка соответствие типов при присваивании
             assigned_type = self.get_expression_type(node.expr)
             declared_type = self.symbol_table[var_id][-1].type
-            if assigned_type != declared_type:
-                print(var_id, node.expr)
+            if assigned_type != declared_type and dec_type_assosiations.get(declared_type, '-') != assigned_type:
+                # print(var_id, node.expr)
+
                 raise ValueError(f"Type mismatch in assignment for variable '{var_id}'. Expected '{declared_type}', found '{assigned_type}'.")
         elif isinstance(node, BinOp):
             # соответствие типов при бинарной операции
@@ -92,12 +103,12 @@ class SemanticAnalyzer:
             for subnode in node.nodes:
                 self.analyze(subnode)
         elif isinstance(node, For):
-            print(node.init, node.cond, node.block, node.where.value)
+            # print(node.init, node.cond, node.block, node.where.value)
             self.analyze(node.init)
             if self.symbol_table[node.init.id_.value][-1].type != 'integer':
                 raise ValueError("Expected integer type in 'For' loop assignment")
             
-            print(type(node.cond))
+            # print(type(node.cond))
             if isinstance(node.cond, Int):
                 pass
             elif isinstance(node.cond, Int):
@@ -107,10 +118,10 @@ class SemanticAnalyzer:
                 self.analyze(subnode)
         elif isinstance(node, FuncProcCall):
             if node.id_.value in safe_funcs:
-                print('?')
+                # print('?')
                 return None
             else:
-                return self.func_table[self.scope_stack[-1]](node.id_.value)[-1].type
+                return self.func_table[self.scope_stack[-1]][-1].type # (node.id_.value)
                 # raise Exception(f"Not implemented {type(node)}")
         elif isinstance(node, FuncImpl):
             if not self.func_table.get(self.scope_stack[-1]):
@@ -154,7 +165,7 @@ class SemanticAnalyzer:
 
             self.scopes_table[node.id_.value] = [f_var, new_func]
             self.symbol_table[node.id_.value] = [f_var]
-            print(self.scope_stack)
+            # print(self.scope_stack)
 
             # Добавим параметры функции в её scope
 
@@ -164,10 +175,10 @@ class SemanticAnalyzer:
             self.analyze(node.var)
             self.analyze(node.block)
 
-            # for # pop
-            print("CLEANING", node.id_.value)
+            # pop
+            # print("CLEANING", node.id_.value)
             for elem in self.scopes_table[node.id_.value]:
-                print("DELETING", elem)
+                # print("DELETING", elem)
                 if isinstance(elem, ElemVar):
                     self.symbol_table[elem.id].pop()
                 elif isinstance(elem, FuncVar):
@@ -190,7 +201,7 @@ class SemanticAnalyzer:
             
             new_func = FuncVar(
                 node.id_.value, 
-                'None', 
+                "None", 
                 self.scope_stack[-1],
                 params_list
             )
@@ -206,31 +217,33 @@ class SemanticAnalyzer:
 
             self.scope_stack.append(node.id_.value)
 
-            # f_var = ElemVar(node.id_.value, node.type_.value, self.scope_stack[-1]) 
-
+            
+            # Вызов самой себя (для рекуксии)
             new_func = FuncVar(
                 node.id_.value, 
-                'None', 
+                "None", 
                 self.scope_stack[-1],
                 params_list
             )
+
             self.func_table[node.id_.value].append(new_func)
 
             self.scopes_table[node.id_.value] = [new_func]
             self.symbol_table[node.id_.value] = []
-            print(self.scope_stack)
+            # print(self.scope_stack)
 
             # Добавим параметры функции в её scope
 
             for var_decl in node.params.params:
                 self.analyze(var_decl)
-            
+
             self.analyze(node.var)
             self.analyze(node.block)
 
-
             # for # pop
+            # print("CLEANING", node.id_.value)
             for elem in self.scopes_table[node.id_.value]:
+                # print("DELETING", elem)
                 if isinstance(elem, ElemVar):
                     self.symbol_table[elem.id].pop()
                 elif isinstance(elem, FuncVar):
@@ -241,8 +254,12 @@ class SemanticAnalyzer:
             del self.scopes_table[node.id_.value]
 
             self.scope_stack.pop()
+        elif isinstance(node, RepeatUntil):
+            self.analyze(node.block)
+            ct = self.get_expression_type(node.cond)
+            assert(ct == 'boolean')
         else:
-            raise Exception(f"Not implemented {type(node)}") #return True
+            raise Exception(f"Not implemented {type(node)}. {node.value}") #return True
 
     def process_bin_op(self, node) -> str:
         numeric = ['integer', 'real']
@@ -252,7 +269,7 @@ class SemanticAnalyzer:
         # print(node.symbol, node.first, node.second)
         # if node.symbol in ['==']:
 
-        if node.symbol in ['<', '>', '<=', '>=', '<>', '==']:
+        if node.symbol in ['<', '>', '<=', '>=', '<>', '=']:
             first_type = self.get_expression_type(node.first)
             second_type = self.get_expression_type(node.second)  
                 
@@ -292,6 +309,15 @@ class SemanticAnalyzer:
             if second_type != 'integer':
                 raise ValueError(f"Type mismatch in '{node.symbol}'. Only integer types allowed, found '{second_type}'.")
             return 'integer'
+        
+        elif node.symbol in ['and', 'or']:
+            first_type = self.get_expression_type(node.first)
+            second_type = self.get_expression_type(node.second)
+            if first_type != 'boolean':
+                raise ValueError(f"Type mismatch in '{node.symbol}'. Only boolean types allowed, found '{first_type}'.")
+            if second_type != 'boolean':
+                raise ValueError(f"Type mismatch in '{node.symbol}'. Only boolean types allowed, found '{second_type}'.")
+            return 'boolean'
 
         raise ValueError(f"Not implemented {node.symbol}")
         # return 'boolean'
@@ -302,13 +328,74 @@ class SemanticAnalyzer:
                 raise ValueError(f"Func or Proc '{expr.id_.value}' is used without declaration.")
             
             # Проверка количества аргументов функции
+            sc = self.func_table[expr.id_.value][-1].scope
+
+            for i in range(len(self.func_table[expr.id_.value])):
+                if self.func_table[expr.id_.value][-i - 1].scope != sc:
+                    break
+                try:
+                    assert len(expr.args.args) == len(self.func_table[expr.id_.value][-i -1].params), f"Invalid parameter amount to {expr.id_.value}"
+
+                    # Проверка типов переданных аргументов
+                    for a, p in zip(expr.args.args, self.func_table[expr.id_.value][-i-1].params):
+                        if isinstance(a, BinOp):
+                            res = self.process_bin_op(a)
+                            assert(res == p), f"Type missmatch in {expr.id_.value} call, {res} and {p}"
+                            break
+                        elif isinstance(a, Char):
+                            assert('char' == p), f"Type missmatch in {expr.id_.value} call, char and {p}"
+                            break
+                        elif isinstance(a, Int):
+                            assert('integer' == p), f"Type missmatch in {expr.id_.value} call"
+                            break
+                        elif isinstance(a, Boolean):
+                            assert('boolean' == p), f"Type missmatch in {expr.id_.value} call"
+                            break
+                        elif isinstance(a, String):
+                            assert('string' == p), f"Type missmatch in {expr.id_.value} call"
+                            break
+                        elif isinstance(a, Real):
+                            assert('real' == p), f"Type missmatch in {expr.id_.value} call"
+                            break
+                        else:
+                            assert(self.symbol_table[a.value][-1].type == p), f"Type missmatch in {expr.id_.value} call"
+                            break
+
+                    return self.func_table[expr.id_.value][-1].type
+                except Exception:
+                    continue
+            
             assert len(expr.args.args) == len(self.func_table[expr.id_.value][-1].params), f"Invalid parameter amount to {expr.id_.value}"
 
             # Проверка типов переданных аргументов
             for a, p in zip(expr.args.args, self.func_table[expr.id_.value][-1].params):
-                assert(self.symbol_table[a.value][-1].type == p), f"Type missmatch in {expr.id_.value} call"
+                if isinstance(a, BinOp):
+                    res = self.process_bin_op(a)
+                    assert(res == p), f"Type missmatch in {expr.id_.value} call, {res} and {p}"
+                    break
+                elif isinstance(a, Char):
+                    assert('char' == p), f"Type missmatch in {expr.id_.value} call, char and {p}"
+                    break
+                elif isinstance(a, Int):
+                    assert('integer' == p), f"Type missmatch in {expr.id_.value} call"
+                    break
+                elif isinstance(a, Boolean):
+                    assert('boolean' == p), f"Type missmatch in {expr.id_.value} call"
+                    break
+                elif isinstance(a, String):
+                    assert('string' == p), f"Type missmatch in {expr.id_.value} call"
+                    break
+                elif isinstance(a, Real):
+                    assert('real' == p), f"Type missmatch in {expr.id_.value} call"
+                    break
+                else:
+                    print(a, p)
+                    assert(self.symbol_table[a.value][-1].type == p), f"Type missmatch in {expr.id_.value} call"
+                    break
 
             return self.func_table[expr.id_.value][-1].type
+
+
         if isinstance(expr, Id):
             # Получите тип переменной из таблицы символов
             var_id = expr.value
