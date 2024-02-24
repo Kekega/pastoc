356278c0bcef4ae404b92b6b1fff78e421fa1c8a
Anton Saykin <saikin.ao@phystech.edu>
2024-02-24 08:09:44 +0300

    bug fix

--- tests/04/gen.c
+++ tests/04/gen.c
@@ -1,25 +1,7 @@
 #include<stdio.h>
-void check_arm(int x, int cj, int cd, int cs);
 int main() {
-	int j;
-	int cj;
-	int cd;
-	int cs;
-	scanf("%d",&j);
-	cj = j % 10;
-	cd = j / 10 % 10;
-	cs = j / 100 % 10;
-	check_arm(j, cj, cd, cs);
-	}
-void check_arm(int x, int cj, int cd, int cs){
-	int arm;
-	if (x<0){
-		exit;
-		}
-	arm = x == cj*cj*cj+cd*cd*cd+cs*cs*cs;
-	if (arm){
-		printf("YES");
-		}else{
-		printf("NO");
-		}
+	int c;
+	c = -1;
+	printf("%d",c);
+	printf("\n");
 	}
